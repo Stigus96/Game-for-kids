624d093992c21f04047766c98409b7bdae9dd315
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-01-21 15:04:54 +0100

    Functional game menu
    The buttons are now active. Something happens when you press them

--- Game menu/TitleScreen.gd
+++ Game menu/TitleScreen.gd
@@ -1,3 +1,20 @@
 #Startmenyú.gd
 extends Control
 
+
+
+func _on_newGame_pressed():
+	$Menu/CenterRow/CenterContainer/Buttons/newGame.grab_focus()
+	get_tree().change_scene("res://ExampleW1.tscn")
+	pass # Replace with function body.
+
+func _on_LevelSelect_pressed():
+	get_tree().change_scene("res://LevelSelect.tscn")
+	pass # Replace with function body.
+
+func _on_Quit_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
+
+
+
