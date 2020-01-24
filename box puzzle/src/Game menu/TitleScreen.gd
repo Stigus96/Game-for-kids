d2dcdc97c0ce61592521b331b940d03e02f5be00
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-01-24 12:28:08 +0100

    Added Background to menu

--- box puzzle/src/Game menu/TitleScreen.gd
+++ box puzzle/src/Game menu/TitleScreen.gd
@@ -1,20 +1,24 @@
-#Startmenyú.gd
+#Startmenu.gd
 extends Control
-
-
 
 func _on_newGame_pressed():
 	$Menu/CenterRow/CenterContainer/Buttons/newGame.grab_focus()
-	get_tree().change_scene("res://ExampleW1.tscn")
+	get_tree().change_scene("res://.tscn")
 	pass # Replace with function body.
 
 func _on_LevelSelect_pressed():
-	get_tree().change_scene("res://LevelSelect.tscn")
+	get_tree().change_scene("res://.tscn")
+	pass # Replace with function body.
+	
+func _on_Options_pressed():
+	get_tree().change_scene("res://.tscn")
 	pass # Replace with function body.
 
 func _on_Quit_pressed():
 	get_tree().quit()
 	pass # Replace with function body.
+
+
 
 
 
