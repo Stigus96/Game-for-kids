18c652f3844021f940a8f1b36506f3088bde7d7d
PetterDesktop <pettersen_97@hotmail.com>
2020-01-25 17:07:25 +0100

    Changed player so it can interact with box

--- box puzzle/src/Actors/Player.gd
+++ box puzzle/src/Actors/Player.gd
@@ -1,11 +1,20 @@
 extends Actor
 
+export var push_speed : = 125.0
 
 func _physics_process(delta: float) -> void:
+	var motion : = Vector2()
+	
+	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction = get_direction()
 	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	
+	if get_slide_count() > 0:
+		check_box_collision(motion)
+		
 	
 	
 func get_direction () -> Vector2:
@@ -29,3 +38,10 @@ func calculate_move_velocity(
 	if is_jump_interrupted:
 		out.y = 0.0
 	return out
+
+func check_box_collision(motion : Vector2) -> void:
+	if abs(motion.x) > 1:
+		return
+	var box : = get_slide_collision(0).collider as box
+	if box:
+		box.push(push_speed * motion)
