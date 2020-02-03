81759116eb4c1b18a31e304cc8fc3ae00a243d71
Daniel Iversen <daiverse@stud.ntnu.no>
2020-02-03 17:52:02 +0100

    Added animation for the coin
    The coin moves up and down as default

--- box puzzle/src/Collectible_Items/Coin.gd
+++ box puzzle/src/Collectible_Items/Coin.gd
@@ -2,7 +2,14 @@ extends Area2D
 
 export var score: = 10
 
-func _on_Coin_body_entered(body):
+var fade_out = "fade_out"
+
+#onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
+
+onready var ANI = get_node("AnimationPlayer")
+
+func _on_Coin_body_entered(body) -> void:
+	ANI.play(fade_out)
 	picked()
 	pass # Replace with function body.
 	
