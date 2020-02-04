dea652a0b5841d052451f1ae3fff1c17073704eb
Daniel Iversen <daiverse@stud.ntnu.no>
2020-02-04 14:17:00 +0100

    Added animations and ability to pick up donut
    Animation for the candy and the donut

--- box puzzle/src/Collectible_Items/Candy.gd
+++ box puzzle/src/Collectible_Items/Candy.gd
@@ -1,18 +1,14 @@
 extends Area2D
 
 export var score: = 50
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 
 
-func _on_Cake_body_entered(body):
+onready var playerAnimation = get_node("AnimationPlayer")
+
+func _on_Cake_body_entered(body) -> void:
+	playerAnimation.play("fade_out")
 	picked()
 	pass # Replace with function body.
 	
 func picked() -> void:
 	PlayerData.score += score
-	queue_free()
--- box puzzle/src/Collectible_Items/Donut.gd
+++ box puzzle/src/Collectible_Items/Donut.gd
@@ -1,15 +1,14 @@
 extends Area2D
 
-export var score: = 10
+export var healthPoints: = 5
+
+var fade_out = "fade_out"
+
+#onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 
 onready var playerAnimation = get_node("AnimationPlayer")
 
-func _on_Coin_body_entered(body) -> void:
-	
+func _on_Donut_body_entered(body) -> void: 
 	if body.name == "Player":
 		playerAnimation.play("fade_out")
-		picked()
 	pass # Replace with function body.
-	
-func picked() -> void:
-	PlayerData.score += score
