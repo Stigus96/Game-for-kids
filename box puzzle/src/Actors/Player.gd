d279381d45360d8f5cbe6303efcfeec8552daee5
stigus96 <stighelgebruset@msn.com>
2020-02-06 19:03:15 +0100

    Added walking and idle animation
    also changed the path of the player files to expand folder to have animations. also adjusted collision box of player slightly

--- box puzzle/src/Actors/Player.gd
+++ box puzzle/src/Actors/Player.gd
@@ -1,10 +1,13 @@
 extends Actor
 
 export var push_speed : = 125.0
+onready var AnimatedPlayer = get_node("AnimatedPlayer")
 
+
+		
 func _physics_process(delta: float) -> void:
 	var motion : = Vector2()
-	
+	animate_player()
 	motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
 	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
@@ -22,6 +25,7 @@ func get_direction () -> Vector2:
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
+		
 		
 		
 func calculate_move_velocity(
@@ -45,3 +49,14 @@ func check_box_collision(motion : Vector2) -> void:
 	var box : = get_slide_collision(0).collider as box
 	if box:
 		box.push(push_speed * motion)
+		
+func animate_player():
+	var direction = get_direction()
+	if (direction.x < 0):
+		AnimatedPlayer.play("walk_left")
+	if (direction.x > 0):
+		 AnimatedPlayer.play("walk_right")
+	if(direction.x == 0):
+		AnimatedPlayer.play("idle")
+
+
