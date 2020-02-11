56ed539f59b8dfc4b0c34acfd4b92fbae7a7c755
Daniel Iversen <daiverse@stud.ntnu.no>
2020-02-11 13:02:28 +0100

    Score Counter
    Changed font. Score gets reset when you reset the level

---
+++ box puzzle/KinematicBody2D2.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+class_name BOX
+
+export var speed: = Vector2(300.0, 100.0)
+export var gravity: = 100.0
+
+var velocity: = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	
+	for i in get_slide_count():
+    var collision = get_slide_collision(i)
+    print("Collided with: ", collision.collider.name)
--- box puzzle/src/KillPlayer/KillFloor.gd
+++ box puzzle/src/KillPlayer/KillFloor.gd
@@ -2,5 +2,6 @@ extends Area2D
 
 func _on_KillFloor_body_entered(body):
 	if body.name == "Player":
+		PlayerData.score = 0
 		get_tree().reload_current_scene()
 
