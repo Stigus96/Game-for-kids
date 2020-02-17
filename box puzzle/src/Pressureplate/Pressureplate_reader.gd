0ba203f6a5a44fc5a9c885ea62c99efea6da620f
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-02-17 11:33:47 +0100

    Updated grass level
    Updated the value of player, more tiles and pausescreen

--- box puzzle/src/Pressureplate/Pressureplate_reader.gd
+++ box puzzle/src/Pressureplate/Pressureplate_reader.gd
@@ -1,11 +1,22 @@
 extends Area2D
 
+onready var tileWall = get_node("TileMap5")
 
 func _ready():
 	pass # Replace with function body.
 
 func _on_Pressureplate_reader_body_entered(body):
 	if body.name == "Player" || body.name == "PhysicsBox2":
-		print("hei")
-		queue_free()
+		tileWall.hide()
+		tileWall.set_collision_layer(2)
+		tileWall.set_collision_mask(2)
 	pass # Replace with function body.
+
+
+func _on_Pressureplate_reader_body_exited(body):
+	if body.name == "Player" || body.name == "PhysicsBox2":
+		tileWall.show()
+		tileWall.set_collision_layer(1)
+		tileWall.set_collision_mask(1)
+	pass # Replace with function body.
+	
