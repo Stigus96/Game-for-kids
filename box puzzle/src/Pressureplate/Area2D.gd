f9e26d81133bc58e08d742dc0913f74c3ba33219
Daniel Iversen <daiverse@stud.ntnu.no>
2020-02-19 09:09:28 +0100

    Multible walls
    Added ability to have multible walls in one level

--- box puzzle/src/Pressureplate/Area2D.gd
+++ box puzzle/src/Pressureplate/Area2D.gd
@@ -2,11 +2,15 @@ extends Area2D
 
 onready var Plate = get_node("Pressureplate_passive")
 
+onready var childer = self.get_child(3)
+
 func _ready():
 	pass # Replace with function body.
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Player" || body.name == "PhysicsBox2":
+		print(childer.get_door_value())
+		childer.find_door(true)
 		Plate.hide()
 		PlayerData.set_door(false)
 	pass # Replace with function body.
@@ -14,6 +18,8 @@ func _on_Area2D_body_entered(body):
 
 func _on_Area2D_body_exited(body):
 	if body.name == "Player" || body.name == "PhysicsBox2":
+		print(childer.get_door_value())
 		Plate.show()
 		PlayerData.set_door(true)
-	pass # Replace with function body.
+		childer.find_door(false)
+	pass # Replace with function body
