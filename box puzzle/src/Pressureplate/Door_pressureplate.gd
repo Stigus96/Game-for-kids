3b05dd91b4d8a3fc79810e72b7dd18b6941ba562
Daniel Iversen <daiverse@stud.ntnu.no>
2020-02-20 10:41:48 +0100

    Pressureplate with spikes
    You can now make the spikes go away and come back when you step on the pressureplate

---
+++ box puzzle/src/Pressureplate/Door_pressureplate.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+onready var Plate = get_node("Pressureplate_passive")
+
+onready var child = get_child(0)
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_Area2D_body_entered(body):
+	if body is KinematicBody2D:
+		child.find_door(true)
+		Plate.hide()
+		PlayerData.set_door(false)
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_exited(body):
+	if body is KinematicBody2D:
+		Plate.show()
+		PlayerData.set_door(true)
+		child.find_door(false)
+	pass # Replace with function body
--- box puzzle/src/Pressureplate/Spike_pressureplate.gd
+++ box puzzle/src/Pressureplate/Spike_pressureplate.gd
@@ -2,23 +2,18 @@ extends Area2D
 
 onready var Plate = get_node("Pressureplate_passive")
 
-onready var childer = self.get_child(4)
+onready var child = get_child(4)
 
 func _ready():
 	pass # Replace with function body.
 
 func _on_Area2D_body_entered(body):
 	if body is KinematicBody2D:
-		childer.find_door(true)
-		Plate.hide()
-		PlayerData.set_door(false)
+		child.remove_spikes()
 	pass # Replace with function body.
 
 
 func _on_Area2D_body_exited(body):
 	if body is KinematicBody2D:
-		print(childer.get_door_value())
-		Plate.show()
-		PlayerData.set_door(true)
-		childer.find_door(false)
+		child.show_spikes()
 	pass # Replace with function body
