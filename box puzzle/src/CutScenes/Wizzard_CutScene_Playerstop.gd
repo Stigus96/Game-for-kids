0d6378c41af0855f51af0680baf4ca272c933d00
Daniel Iversen <daiverse@stud.ntnu.no>
2020-03-02 16:26:22 +0100

    Minor fixes with wizzard cutscene

--- box puzzle/src/CutScenes/Wizzard_CutScene_Playerstop.gd
+++ box puzzle/src/CutScenes/Wizzard_CutScene_Playerstop.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 onready var PlayerNode = get_parent()
+onready var Wizzard = get_child(1)
 
 
 func _ready():
@@ -10,4 +11,5 @@ func _ready():
 func _on_Area2D_body_entered(body):
 	PlayerNode.get_node("Player").update_playerAutoMov(false)
 	print(PlayerNode)
+	
 	pass # Replace with function body.
