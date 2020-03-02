3894809d4b055dff95a94a0c579fe44630dc7b3a
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-03-02 12:55:37 +0100

    Added teleport sound
    Added teleport sound when entering portal. Moved top box at grass level

--- box puzzle/src/Portal/Portal.gd
+++ box puzzle/src/Portal/Portal.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 export var next_scene: PackedScene
 
-onready var music = get_node("../AudioStreamPlayer")
+onready var musicFade = get_node("../AudioStreamPlayer")
 
 onready var minimumVolume = -80
 onready var myVolume = 0
@@ -10,20 +10,21 @@ onready var myVolume = 0
 
 func _on_Portal_body_entered(body):
 	fade_out_music()
+	$AudioStreamPlayer.play()
 	pass # Replace with function body.
 	
 func teleport() -> void:
 	get_tree().change_scene_to(next_scene)
 
 func fade_out_music() -> void:
-	var currentVolume = music.get_volume_db()
+	var currentVolume = musicFade.get_volume_db()
 	while currentVolume > minimumVolume :
 		currentVolume = currentVolume - 12
-		myVolume = music.set_volume_db(currentVolume)
+		myVolume = musicFade.set_volume_db(currentVolume)
 		print(currentVolume)
 		yield(get_tree().create_timer(0.1), "timeout")
 		if currentVolume <= -80:
 			print(currentVolume)
-			music.stop()
+			musicFade.stop()
 			teleport()
 
