2342033702143de8e03f8ad64f3c492e948aa5ad
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-03-02 16:24:44 +0100

    Added a return button to the menu

--- box puzzle/src/Game menu/LevelSelect.gd
+++ box puzzle/src/Game menu/LevelSelect.gd
@@ -1,25 +1,27 @@
 extends Control
 
-func _physics_process(delta):
-	if $TextureRect/tutorialBtn.is_hovered() == true:
-		$TextureRect/tutorialBtn.grab_focus()
-	
-	if $TextureRect/grassLevelBtn.is_hovered() == true:
-		$TextureRect/grassLevelBtn.grab_focus()
-	
-	if $TextureRect/caveLevelBtn.is_hovered():
-		$TextureRect/caveLevelBtn.grab_focus()
-	
-	if $TextureRect/TBDBtn.is_hovered() == true:
-		$TextureRect/TBDBtn.grab_focus()
-	
-	if $TextureRect/TBDBtn2.is_hovered() == true:
-		$TextureRect/TBDBtn2.grab_focus()
-	
-	if $TextureRect/candylandBtn.is_hovered() == true:
-		$TextureRect/candylandBtn.grab_focus()
-
-
+#func _physics_process(delta):
+#	if $TextureRect/tutorialBtn.is_hovered() == true:
+#		$TextureRect/tutorialBtn.grab_focus()
+#
+#	if $TextureRect/grassLevelBtn.is_hovered() == true:
+#		$TextureRect/grassLevelBtn.grab_focus()
+#
+#	if $TextureRect/caveLevelBtn.is_hovered() == true:
+#		$TextureRect/caveLevelBtn.grab_focus()
+#
+#	if $TextureRect/TBDBtn.is_hovered() == true:
+#		$TextureRect/TBDBtn.grab_focus()
+#
+#	if $TextureRect/TBDBtn2.is_hovered() == true:
+#		$TextureRect/TBDBtn2.grab_focus()
+#
+#	if $TextureRect/candylandBtn.is_hovered() == true:
+#		$TextureRect/candylandBtn.grab_focus()
+#
+#func _input(event):
+#	if event.get_local_scene():
+#		$TextureRect/tutorialBtn.grab_focus()
 
 func _on_tutorialBtn_pressed():
 	get_tree().change_scene("res://src/Levels/Tutorial.tscn")
@@ -44,5 +46,7 @@ func _on_TBDBtn2_pressed():
 func _on_candylandBtn_pressed():
 	get_tree().change_scene("")
 	pass
+
+
 
 
