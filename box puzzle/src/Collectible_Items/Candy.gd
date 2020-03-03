f29a31025fff7def9a55457efd8e7e9325a2776e
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-03-03 14:56:27 +0100

    added chewing sound when eating candy

--- box puzzle/src/Collectible_Items/Candy.gd
+++ box puzzle/src/Collectible_Items/Candy.gd
@@ -5,8 +5,10 @@ export var player_speed: = 1
 
 
 onready var playerAnimation = get_node("AnimationPlayer")
+onready var chewingSound = get_node("AudioStreamPlayer")
 
 func _on_Cake_body_entered(body) -> void:
+	chewingSound.play()
 	playerAnimation.play("fade_out")
 	player_speed = player_speed + 1
 	picked()
