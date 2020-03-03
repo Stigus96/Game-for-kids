875b5ecba120b2fe7b46b05c47f6a6dc8ceff09b
Petter <Pettersen_97@hotmail.com>
2020-03-03 13:41:20 +0100

    Made candy level ready for developer

--- box puzzle/src/CandyLevelObstacles/KinematicBody2D.gd
+++ box puzzle/src/CandyLevelObstacles/KinematicBody2D.gd
@@ -6,5 +6,5 @@ var state = 0
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
-	motion.x = 300
+	motion.x = 350
 	move_and_slide(motion, Vector2(0,-1))
