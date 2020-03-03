f0c4aeac69e2093533b15dbb1128a690715cd5b8
Petter <Pettersen_97@hotmail.com>
2020-03-03 12:55:50 +0100

    Added horse obstacles that change the speed

--- box puzzle/HesteHinder.gd
+++ box puzzle/HesteHinder.gd
@@ -1,15 +1,23 @@
 extends Area2D
 export var player_speed: = 1
 
+onready var hinder_down = get_node("Sprite")
+onready var hinder_up = get_node("pixil-frame-0")
+
 
 onready var playerAnimation = get_node("AnimationPlayer")
-
-func _on_Cake_body_entered(body) -> void:
-	playerAnimation.play("fade_out")
-	player_speed = player_speed + 1
-	picked()
-	pass # Replace with function body.
 	
 func picked() -> void:
 	PlayerData.update_player_speed(false)
 
+
+
+func _on_HesteHinder_body_entered(body):
+	if body.name == "Horse":
+		playerAnimation.play("fade_out")
+		player_speed = player_speed + 1
+		picked()
+		
+		hinder_up.hide()
+		hinder_down.show()
+	pass # Replace with function body.
