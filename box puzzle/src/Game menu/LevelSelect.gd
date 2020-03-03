4c42cf83d74a8b4863c3f02ad120ba9cb7cb7540
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-03-03 14:02:25 +0100

    added return button
    Added return button and to level select and updated score counter

--- box puzzle/src/Game menu/LevelSelect.gd
+++ box puzzle/src/Game menu/LevelSelect.gd
@@ -1,28 +1,5 @@
 extends Control
 
-#func _physics_process(delta):
-#	if $TextureRect/tutorialBtn.is_hovered() == true:
-#		$TextureRect/tutorialBtn.grab_focus()
-#
-#	if $TextureRect/grassLevelBtn.is_hovered() == true:
-#		$TextureRect/grassLevelBtn.grab_focus()
-#
-#	if $TextureRect/caveLevelBtn.is_hovered() == true:
-#		$TextureRect/caveLevelBtn.grab_focus()
-#
-#	if $TextureRect/TBDBtn.is_hovered() == true:
-#		$TextureRect/TBDBtn.grab_focus()
-#
-#	if $TextureRect/TBDBtn2.is_hovered() == true:
-#		$TextureRect/TBDBtn2.grab_focus()
-#
-#	if $TextureRect/candylandBtn.is_hovered() == true:
-#		$TextureRect/candylandBtn.grab_focus()
-#
-#func _input(event):
-#	if event.get_local_scene():
-#		$TextureRect/tutorialBtn.grab_focus()
-
 func _on_tutorialBtn_pressed():
 	get_tree().change_scene("res://src/Levels/Tutorial.tscn")
 	pass
@@ -46,7 +23,4 @@ func _on_TBDBtn2_pressed():
 func _on_candylandBtn_pressed():
 	get_tree().change_scene("")
 	pass
-
-
-
 
--- box puzzle/src/Game menu/ReturnButton.gd
+++ box puzzle/src/Game menu/ReturnButton.gd
@@ -1,5 +1,19 @@
 extends Control
 
+onready var yellowBtn = get_node("Sprite")
+
+func _ready():
+	yellowBtn.hide()
+	
 func _on_Button_pressed():
+	yellowBtn.show()
 	get_tree().change_scene("res://src/Game menu/TitleScreen.tscn")
+	pass # Replace with function body.
+	
+func _on_Button_mouse_entered():
+	yellowBtn.show()
+	pass # Replace with function body.
+	
+func _on_Button_mouse_exited():
+	yellowBtn.hide()
 	pass # Replace with function body.
