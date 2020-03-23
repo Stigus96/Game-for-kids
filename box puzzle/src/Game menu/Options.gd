7e419a20ac1ded0e153cc7187bdf028761635e1e
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-03-23 13:57:23 +0100

    Added fullscreen option to the menu

--- box puzzle/src/Game menu/Options.gd
+++ box puzzle/src/Game menu/Options.gd
@@ -4,7 +4,6 @@ var music = 0
 var sound = 0
 var masterVolume = 0
 
-
 func _on_MasterSound_value_changed(value):
 	masterVolume = value
 	AudioServer.set_bus_volume_db(0, masterVolume)
@@ -21,4 +20,8 @@ func _on_ObjectSound_value_changed(value):
 	sound = value
 	AudioServer.set_bus_volume_db(2, sound)
 	print(AudioServer.get_bus_volume_db(2))
+	pass # Replace with function body.
+
+func _on_CheckBox_pressed():
+	OS.window_fullscreen = !OS.window_fullscreen
 	pass # Replace with function body.
