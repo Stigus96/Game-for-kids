6a391b11945606be924f7808bfefe39c675f8fb8
PetterDesktop <pettersen_97@hotmail.com>
2020-04-02 16:10:22 +0200

    Made adjustment to cutscene portal

--- box puzzle/src/CutScenes/CutScene_nextScene.gd
+++ box puzzle/src/CutScenes/CutScene_nextScene.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
+export var candyLevel: PackedScene
 
 func _on_Area2D_body_entered(body):
 	if body.name == "Horse":
-		get_tree().change_scene("res://src/Levels/Candyland.tscn")
+		get_tree().change_scene_to(candyLevel)
 	pass # Replace with function body.
