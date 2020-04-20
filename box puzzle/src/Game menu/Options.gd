60e022158db36216c2f34cfdc5e785b8d423c7eb
Sverre Lofthus <sverrlof@stud.ntnu.no>
2020-04-20 12:47:57 +0200

    Small change in options script

--- box puzzle/src/Game menu/Options.gd
+++ box puzzle/src/Game menu/Options.gd
@@ -7,7 +7,7 @@ var masterVolume = 0
 func _on_MasterSound_value_changed(value):
 	masterVolume = value
 	AudioServer.set_bus_volume_db(0, masterVolume)
-	#print(AudioServer.get_bus_volume_db(0))
+	print(AudioServer.get_bus_volume_db(0))
 	pass # Replace with function body.
 	
 func _on_Music_value_changed(value):
@@ -18,7 +18,7 @@ func _on_Music_value_changed(value):
 func _on_ObjectSound_value_changed(value):
 	sound = value
 	AudioServer.set_bus_volume_db(2, sound)
-	#print(AudioServer.get_bus_volume_db(2))
+	print(AudioServer.get_bus_volume_db(2))
 	pass # Replace with function body.
 
 func _on_CheckBox_pressed():
