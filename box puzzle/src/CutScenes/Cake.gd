97db33c2ca6a9e6c7f897abd3d5e0dccc72d88c8
Daniel Iversen <daiverse@stud.ntnu.no>
2020-05-12 23:02:26 +0200

    MergeReady

--- box puzzle/src/CutScenes/Cake.gd
+++ box puzzle/src/CutScenes/Cake.gd
@@ -7,4 +7,4 @@ onready var is_bird_finished_flying = false
 #When the bird from the cutscene enters the cake body, an aniation is played for the cake
 func _on_Cake_body_entered(body):
 	if body.name == "Bird":
-		cake_animation.play("Fly_Away")
+		cake_animation.play("Fly_Away") #Plays new animation
